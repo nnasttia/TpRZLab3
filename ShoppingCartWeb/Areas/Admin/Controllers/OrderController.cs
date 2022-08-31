0bd0f31b65b95aea59b3ad872c7812d697feed9f
Ruslan Krolevets <rkrolevets@gmail.com>
2022-08-31 19:50:38 +0300

    Add project files.

---
+++ ShoppingCartWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.DataAccess.Repositories;
+using ShoppingCart.DataAccess.ViewModels;
+using ShoppingCart.Models;
+using ShoppingCart.Utility;
+using Stripe;
+using Stripe.Checkout;
+using System.Security.Claims;
+
+namespace ShoppingCart.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region APICALL
+        public IActionResult AllOrders(string status)
+        {
+            IEnumerable<OrderHeader> orderHeader;
+            if (User.IsInRole("Admin") || User.IsInRole("Employee"))
+                orderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeader = _unitOfWork.OrderHeader.GetAll(x => x.ApplicationUserId == claims.Value);
+            }
+            switch (status)
+            {
+                case "pending":
+                    orderHeader = orderHeader.Where(x => x.PaymentStatus == PaymentStatus.StatusPending);
+                    break;
+                case "approved":
+                    orderHeader = orderHeader.Where(x => x.PaymentStatus == PaymentStatus.StatusApproved);
+                    break;
+                case "underprocess":
+                    orderHeader = orderHeader.Where(x => x.OrderStatus == OrderStatus.StatusInProcess);
+                    break;
+                case "shipped":
+                    orderHeader = orderHeader.Where(x => x.OrderStatus == OrderStatus.StatusShipped);
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = orderHeader });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OrderDetails(int id)
+        {
+            OrderVM orderVM = new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == id, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetail.GetAll(x => x.OrderHeaderId == id, includeProperties: "Product")
+            };
+            return View(orderVM);
+        }
+
+        [Authorize(Roles = WebSiteRole.Role_Admin + "," + WebSiteRole.Role_Employee)]
+        [HttpPost]
+        public IActionResult OrderDetails(OrderVM vm)
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == vm.OrderHeader.Id);
+            orderHeader.Name = vm.OrderHeader.Name;
+            orderHeader.Phone = vm.OrderHeader.Phone;
+            orderHeader.Address = vm.OrderHeader.Address;
+            orderHeader.City = vm.OrderHeader.City;
+            orderHeader.State = vm.OrderHeader.State;
+            orderHeader.PostalCode = vm.OrderHeader.PostalCode;
+            if (vm.OrderHeader.Carrier != null)
+                orderHeader.Carrier = vm.OrderHeader.Carrier;
+            if (vm.OrderHeader.TrackingNumber != null)
+                orderHeader.TrackingNumber = vm.OrderHeader.TrackingNumber;
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Info Updated";
+            return RedirectToAction("OrderDetails", "Order", new { id = vm.OrderHeader.Id });
+        }
+
+        [Authorize(Roles = WebSiteRole.Role_Admin + "," + WebSiteRole.Role_Employee)]
+        public IActionResult InProcess(OrderVM vm)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(vm.OrderHeader.Id, OrderStatus.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["success"] = "Order status updated to Inprocess";
+            return RedirectToAction("OrderDetails", "Order", new { id = vm.OrderHeader.Id });
+        }
+
+        [Authorize(Roles = WebSiteRole.Role_Admin + "," + WebSiteRole.Role_Employee)]
+        public IActionResult Shipped(OrderVM vm)
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == vm.OrderHeader.Id);
+            orderHeader.Carrier = vm.OrderHeader.Carrier;
+            orderHeader.TrackingNumber = vm.OrderHeader.TrackingNumber;
+            orderHeader.OrderStatus = OrderStatus.StatusShipped;
+            orderHeader.DateOfShipping = DateTime.Now;
+
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Order status updated to Shipped";
+            return RedirectToAction("OrderDetails", "Order", new { id = vm.OrderHeader.Id });
+        }
+        [Authorize(Roles = WebSiteRole.Role_Admin + "," + WebSiteRole.Role_Employee)]
+        public IActionResult CancelOrder (OrderVM vm)
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == vm.OrderHeader.Id);
+            if (orderHeader.PaymentStatus == PaymentStatus.StatusApproved)
+            {
+                var refundOptions = new RefundCreateOptions()
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderHeader.PaymentIntentId
+                };
+                var service = new RefundService();
+                Refund refund = service.Create(refundOptions);
+                _unitOfWork.OrderHeader.UpdateStatus(vm.OrderHeader.Id, OrderStatus.StatusCancelled);
+            }
+            else
+            {
+                _unitOfWork.OrderHeader.UpdateStatus(vm.OrderHeader.Id, OrderStatus.StatusCancelled);
+            }
+            _unitOfWork.Save();
+            TempData["success"] = "Order cancelled";
+            return RedirectToAction("OrderDetails", "Order", new { id = vm.OrderHeader.Id });
+        }
+        public IActionResult PayNow(OrderVM vm)
+        {
+            var orderheader = _unitOfWork.OrderHeader.GetT(x => x.Id == vm.OrderHeader.Id, 
+                includeProperties:"ApplicationUser");
+            var orderDetail = _unitOfWork.OrderDetail.GetAll(x => x.OrderHeaderId == vm.OrderHeader.Id,
+                includeProperties:"Product");
+            var domain = "https://Localhost:7191/";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                SuccessUrl = domain + $"customer/cart/OrderSuccess?id={vm.OrderHeader.Id}",
+                CancelUrl = domain + $"customer/cart/Index",
+            };
+            foreach (var item in orderDetail)
+            {
+                var lineItemsOptions = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Product.Price * 100),
+                        Currency = "USD",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Name,
+                        },
+                    },
+                    Quantity = item.Count,
+                };
+                options.LineItems.Add(lineItemsOptions);
+            }
+            var service = new SessionService();
+            Session session = service.Create(options);
+            _unitOfWork.OrderHeader.PaymentStatus(vm.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ShoppingCartWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShoppingCart.DataAccess.Repositories;
+using ShoppingCart.DataAccess.ViewModels;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProductController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        IWebHostEnvironment _hostingEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnvironment = webHostEnvironment;
+        }
+
+        #region APICALL
+        public IActionResult AllProducts()
+        {
+            var products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return Json(new { data = products });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            ProductVM vm = new ()
+            {
+                Product = new(),
+                Categories = _unitOfWork.Category.GetAll().Select(x =>
+                new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            if (id == null || id == 0)
+                return View(vm);
+            else
+            {
+                vm.Product = _unitOfWork.Product.GetT(x => x.Id == id);
+                if (vm.Product == null)
+                    return NotFound();
+                else
+                    return View(vm);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(ProductVM vm, IFormFile? file)
+        {
+            if(ModelState.IsValid)
+            {
+                string fileName = string.Empty;  
+                if (file != null)
+                {
+                    string uploadDir = Path.Combine(_hostingEnvironment.WebRootPath, "ProductImage");
+                    fileName = Guid.NewGuid().ToString() + "-" + file.FileName;
+                    string filePath = Path.Combine(uploadDir, fileName);
+                    if (vm.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(_hostingEnvironment.WebRootPath, 
+                            vm.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                            System.IO.File.Delete(oldImagePath);
+                    }
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    vm.Product.ImageUrl = @"\ProductImage\" + fileName;
+                }
+                if (vm.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(vm.Product);
+                    TempData["success"] = "Product Create Done";
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(vm.Product);
+                    TempData["success"] = "Product Create Done";
+                }
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        #region DeleteAPICALL
+        [HttpDelete]
+        public IActionResult Delete (int? id)
+        {
+            var product = _unitOfWork.Product.GetT(x => x.Id == id);
+            if (product == null)
+                return Json(new { success = false, message = "Error in Fetching Data" });
+            else
+            {
+                var oldImagePath = Path.Combine(_hostingEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath))
+                    System.IO.File.Delete(oldImagePath);
+                _unitOfWork.Product.Delete(product);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Product Deleted" });
+            }
+        }
+        #endregion
+
+    }
+}
